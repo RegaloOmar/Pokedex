5135e84013040bf8032cdc7f8da008b614c4822f
Omar Regalado <regaladomendozaomar@gmail.com>
2024-01-23 12:50:26 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Omar Regalado Mendoza on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
